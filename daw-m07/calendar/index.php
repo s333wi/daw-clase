759b6d2508d2245d92d51cd3707cb34757a6c35a
s333wi <pandoloraw@gmail.com>
2022-10-05 13:28:30 +0200

    Pa clase

--- daw-m07/calendar/index.php
+++ daw-m07/calendar/index.php
@@ -9,15 +9,17 @@ $abbrDies = ['Dl', 'Dm', 'Dc', 'Dj', 'Dv', 'Ds', 'Dg'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
+    <link rel="stylesheet" type='text/css' href='./style.css' />
+
 </head>
 
 <body>
-    <table>
+    <table border=1>
         <thead>
             <tr>
                 <td>
                     << </td>
-                <td>
+                <td colspan=2 class="noBorder">
                     <select name="months">
                         <?php
                         for ($month = 1; $month <= 12; $month++) {
@@ -27,14 +29,15 @@ $abbrDies = ['Dl', 'Dm', 'Dc', 'Dj', 'Dv', 'Ds', 'Dg'];
                         ?>
                     </select>
                 </td>
-                <td>
-                    <select name="years">
-                        <?php
-                        for ($year = 1970; $year <= 2077; $year++) {
-                            $dia = date("Y", strtotime("01-01-" . $year));
-                            echo "<option value='" . $dia . "'>" . $dia . "</option>";
-                        }
-                        ?>
+                <td class="noBorder"></td>
+                <td colspan=2 class="noBorder">
+                <select name=" years">
+                    <?php
+                    for ($year = 1970; $year <= 2077; $year++) {
+                        $dia = date("Y", strtotime("01-01-" . $year));
+                        echo "<option value='" . $dia . "'>" . $dia . "</option>";
+                    }
+                    ?>
                     </select>
                 </td>
                 <td> >> </td>
