d525be8bf21f22725b760fc6497bbc0da9f4e0c3
s333wi <pandoloraw@gmail.com>
2022-10-16 14:24:26 +0200

    "Final validator"

--- daw-m06/loginValidation/check.js
+++ daw-m06/loginValidation/check.js
@@ -1,42 +1,38 @@
 var errorDiv;
+const obj_errors = {
+    errorEmail: "L'email no es valid",
+    errorPassLength: "La contrasenya ha de ser mes gran de 9 caracters",
+    errorPassNumber: "El password ha de contenir minim 1 numero",
+    errorPassUpper: "El password ha de contenir minim 2 majuscules",
+    errorPassMatch: "Els passwords no coincideixen"
+};
 document.getElementById("btnSubmit").addEventListener("click", function (event) {
-    
+
     event.preventDefault();
+    errorDiv = document.querySelector("#inpCheck");
     let email = document.querySelector('#formEmail');
     let password = document.getElementById("formPass");
     let confirmPassword = document.getElementById("formPassConfirm");
     let passLength = password.value.length >= 9;
-    let errorEmail = document.getElementById("errorEmail");
-    let errorPassLength = document.getElementById("errorPassLength");
-    let errorPassNumber = document.getElementById("errorPassNumber");
-    let errorPassUpper = document.getElementById("errorPassUpper");
-    let errorPassMatch = document.getElementById("errorPassMatch");
-    (validateEmailAddress(email.value)) ? canRemove(errorEmail) : canAppend('errorEmail', errorEmail);
-    (passLength) ? canRemove(errorPassLength) : canAppend('errorPassLength', errorPassLength);
-    passContainsNumber(password.value) ? canRemove(errorPassNumber) : canAppend('errorPassNumber', errorPassNumber);
-    passHasUpper(password.value) ? canRemove(errorPassUpper) : canAppend('errorPassUpper', errorPassUpper);
-    (password.value === confirmPassword.value) ? canRemove(errorPassMatch) : canAppend('errorPassMatch', errorPassMatch);
+    (validateEmailAddress(email.value)) ? canRemove('errorEmail') : canAppend('errorEmail');
+    (passLength) ? canRemove('errorPassLength') : canAppend('errorPassLength');
+    passContainsNumber(password.value) ? canRemove('errorPassNumber') : canAppend('errorPassNumber');
+    passHasUpper(password.value) ? canRemove('errorPassUpper') : canAppend('errorPassUpper');
+    (password.value === confirmPassword.value) ? canRemove('errorPassMatch') : canAppend('errorPassMatch');
     let errorElements = document.querySelectorAll('p.error');
     if (errorElements.length === 0) {
         document.forms['loginForm'].submit();
     }
 });
 
-function canRemove(child) {
-
-    if (child !== null) {
-        errorDiv.removeChild(child);
+function canRemove(elementId) {
+    let element = document.getElementById(elementId);
+    if (element !== null) {
+        element.remove();
     }
 }
-function canAppend(error, value) {
-    errorDiv = document.querySelector("#inpCheck");
-    const obj_errors = {
-        errorEmail: "L'email no es valid",
-        errorPassLength: "La contrasenya ha de ser mes gran de 9 caracters",
-        errorPassNumber: "El password ha de contenir minim 1 numero",
-        errorPassUpper: "El password ha de contenir minim 2 majuscules",
-        errorPassMatch: "Els passwords no coincideixen"
-    };
+function canAppend(error) {
+    let value = document.getElementById(error);
     errorDiv.innerHTML += (value === null) ? "<p class='error' id='" + error + "'>" + obj_errors[error] + "</p>" : "";
 }
 function validateEmailAddress(emailString) {
