446ebf6b6b5bf7897485d5096b5186cc772d8947
s333wi <pandoloraw@gmail.com>
2022-10-25 20:59:43 +0200

    Agenda

---
+++ daw-m06/agenda/agenda.js
@@ -0,0 +1,9 @@
+let formulari = document.getElementsByClassName('formulari');
+console.log(typeof formulari);
+
+
+Array.from(formulari).forEach(element => {
+
+    let childClone = element.cloneNode();
+    console.log({childClone});
+});
