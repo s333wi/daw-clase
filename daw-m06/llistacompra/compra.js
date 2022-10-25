6f19020d3f3914d14dcf4e60e965a432563136e3
s333wi <pandoloraw@gmail.com>
2022-10-25 16:42:59 +0200

    test

--- daw-m06/llistacompra/compra.js
+++ daw-m06/llistacompra/compra.js
@@ -1,27 +1,30 @@
+var storage = window.localStorage;
+var listaCompra = document.getElementById("listaCompra");
+var arrCompra = [];
+var jsonCompra = storage.getItem("compra");
 window.onload = function () {
-    var storage = window.localStorage;
-    var listaCompra = document.getElementById("listaCompra");
-    var arrCompra = [];
-    var jsonCompra = storage.getItem("compra");
 
-
-    if (jsonCompra !== "null") {
+    alert(jsonCompra);
+    if (jsonCompra !== null) {
         arrCompra = JSON.parse(jsonCompra);
-    }
-    for (let producte of arrCompra) {
-        listaCompra.innerHTML += "<li><span style='visibility:visible'>" + producte + "</span><input type=text style='visibility:hidden' id='" + producte + "'>" +
-            "<button class='btnEdit'>Editar</button><button class='btnDelete'>Borrar</button></li>";
+
+        for (let producte of arrCompra) {
+            listaCompra.innerHTML += "<li><span style='visibility:visible'>" + producte + "</span><input type=text style='visibility:hidden' id='" + producte + "'>" +
+                "<button class='btnEdit'>Editar</button><button class='btnDelete'>Borrar</button></li>";
+        }
     }
 
-    document.getElementById("btnSearch").addEventListener("click", () => {
+    document.getElementById("btnSearch").addEventListener("click", function () {
         var addQuery = document.getElementById("searchQuery");
         alert(addQuery.value);
         arrCompra.push(addQuery.value);
+        console.log({ arrCompra });
+        storage.setItem("compra", JSON.stringify(arrCompra));
     });
 
     const editArr = document.getElementsByClassName("btnEdit");
     for (let btnEdit of editArr) {
-        btnEdit.addEventListener("click", () => {
+        btnEdit.addEventListener("click", function () {
             var btnValue = btnEdit.innerHTML;
             var parent = btnEdit.parentElement;
 
@@ -34,17 +37,14 @@ window.onload = function () {
             } else if (btnValue === "Guardar") {
                 btnEdit.innerHTML = "Editar";
                 console.log(parent.children[1]);
-                parent.children[0].style.visibility = "visible";
-                parent.children[1].style.visibility = "hidden";
+                parent.children[0].style.visibility = "hidden";
+                parent.children[1].style.visibility = "none";
                 let index = arrCompra.indexOf(parent.children[0].innerHTML);
+                console.log(parent.children[1]);
                 arrCompra[index] = parent.children[1].value;
             }
         });
     }
-    console.log(arrCompra);
-    storage.setItem("compra", JSON.stringify(arrCompra));
-    
-    $('.task-delete').click(() => {
-        idfniogf
-    })
+
+
 }
