c586395afeb217c4520413436b7c97677a1c8e84
s333wi <pandoloraw@gmail.com>
2022-12-05 10:51:22 +0100

    Create cards in table

--- daw-m06/UF3/WhereIsDuo_noclass/game.js
+++ daw-m06/UF3/WhereIsDuo_noclass/game.js
@@ -3,6 +3,14 @@ const cardsObj = [
   { id: 2, imgPath: "/resources/frontal2.png" },
   { id: 3, imgPath: "/resources/frontal3.png" },
   { id: 4, imgPath: "/resources/frontal4.png" },
+  { id: 5, imgPath: "/resources/frontal5.png" },
+  { id: 6, imgPath: "/resources/frontal6.png" },
+  { id: 7, imgPath: "/resources/frontal7.png" },
+  { id: 8, imgPath: "/resources/frontal8.png" },
+  { id: 9, imgPath: "/resources/frontal9.png" },
+  { id: 10, imgPath: "/resources/frontal10.png" },
+  { id: 11, imgPath: "/resources/frontal11.png" },
+  { id: 12, imgPath: "/resources/frontal12.png" },
 ];
 
 const cardBackImgPath = "/resources/trasera.png";
@@ -17,10 +25,19 @@ const cardContainerElem = document.querySelector(".card-container");
     </div>
 </div>
 </div> */
+function createRandPos(arr) {
+  for (var i = 3; i <= 24; i++) {
+    arr.push(i);
+  }
+}
+
 let cardsGame = [];
 createCards();
-cards = document.querySelectorAll(".card");
-flipCards(true);
+createCards();
+let cards = document.querySelectorAll(".card");
+let randCardPos = [];
+createRandPos(randCardPos);
+console.log(randCardPos);
 
 //Creem les cartes al tauler
 function createCards() {
@@ -28,6 +45,26 @@ function createCards() {
     createCard(card);
   });
 }
+let cardsFlipped = 0;
+cards.forEach((card) => {
+  card.addEventListener("click", function (e) {
+    let innerCard = card.firstChild;
+    if (innerCard.classList.contains("flip")) {
+      innerCard.classList.remove("flip");
+    } else if (cardsFlipped < 2) {
+      addClassToElement(innerCard, "flip");
+      cardsFlipped++;
+    }
+    if (cardsFlipped === 2) {
+      setTimeout(() => {
+        document.querySelectorAll(".flip").forEach((card) => {
+          card.classList.remove("flip");
+        });
+        cardsFlipped = 0;
+      }, 2000);
+    }
+  });
+});
 
 function createCard(card) {
   //Crear els divs per mostrar una carta
@@ -42,7 +79,6 @@ function createCard(card) {
 
   //Afegir la classe i el id a una carta
   addClassToElement(cardElem, "card");
-  addIdToElement(cardElem, card.id);
 
   addClassToElement(cardInner, "card-inner");
   addClassToElement(cardFront, "card-front");
@@ -89,36 +125,5 @@ function addChildElement(parentElement, childElement) {
 }
 
 function addCardToTable(card) {
-  const cardPositionClass = mapCardIdToTable(card);
-
-  const cardPositionElement = document.querySelector(cardPositionClass);
-
-  addChildElement(cardPositionElement, card);
-}
-
-function mapCardIdToTable(card) {
-  if (card.id == 1) {
-    return ".card-pos-a";
-  } else if (card.id == 2) {
-    return ".card-pos-b";
-  } else if (card.id == 3) {
-    return ".card-pos-c";
-  } else if (card.id == 4) {
-    return ".card-pos-d";
-  }
-}
-
-function flipCard(card, flipToBack) {
-  const innerCardElem = card.firstChild;
-  if (flipToBack && !innerCardElem.classList.contains("flip")) {
-    innerCardElem.classList.add("flip");
-  } else if (innerCardElem.classList.contains("flip")) {
-    innerCardElem.classList.remove("flip");
-  }
-}
-
-function flipCards(flipToBack) {
-  cards.forEach((card, index) => {
-      flipCard(card, flipToBack);
-  });
+  addChildElement(cardContainerElem, card);
 }
